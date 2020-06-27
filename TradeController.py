b4d92954013036b41618aa3efd680f4974ce9f34
elzup <guild0105@gmail.com>
2020-06-27 17:41:03 +0900

    refactor

--- TradeController.py
+++ TradeController.py
@@ -2,6 +2,7 @@ import time
 
 from config import Tradeconfig
 from ExecLogic import ExecLogic
+# from pprint import pprint
 from TradeMethod import TradeMethod
 
 # フラグ設定
@@ -53,88 +54,77 @@ while True:
 
     if thread_flag == buy_jdg:
         # データを取得して買い判定か調べる
-        if logic.buy_judge():
-            amount, price = trader.calc_buy_amount_price()
+        if not logic.buy_judge():
+            continue
+        amount, price = trader.calc_buy_amount_price()
+        result = trader.buy_signal(amount, price, True)
 
-            result = trader.buy_signal(amount, price, True)
-
-            if result[0]:
-                trade_id[0] = True
-                trade_id[1] = result[1]
-                thread_flag = buy_order_check
-                trader.d_message(
-                    "Send buy order\nsize: " +
-                    str(amount) +
-                    "\nprice: " +
-                    str(price))
-            else:
-                trader.d_message(
-                    "TradeController buy_jdg : Failed to send buy signal.")
-                raise Exception(
-                    "TradeController buy_jdg : Failed to send buy signal.")
+        if not result[0]:
+            msg = "TradeController buy_jdg : Failed to send buy signal."
+            trader.d_message(msg)
+            raise Exception(msg)
+        trade_id[0] = True
+        trade_id[1] = result[1]
+        thread_flag = buy_order_check
+        trader.d_message("Send buy order\nsize: " + str(amount) + "\nprice: " + str(price))
 
     elif thread_flag == buy_order_check:
-        if trade_id[0]:
-            res = trader.isCompleted(trade_id[1])
-            if res[0]:
-                trade_id[0] = False
-                if res[1] == "SELL":
-                    trader.d_message(
-                        "TradeController/buy_order_check : Something strange.")
-                    raise Exception(
-                        "TradeController/buy_order_check : Something strange.")
-                elif res[1] == "BUY":
-                    amount = res[3] * comm
-                    price = res[2] * Tradeconfig.sell_rate
+        if not trade_id[0]:
+            msg = "TradeController buy_order_check : Something strange."
+            trader.d_message(msg)
+            raise Exception(msg)
+        res = trader.isCompleted(trade_id[1])
+        if not res[0]:
+            continue
+        trade_id[0] = False
+        if res[1] == "SELL":
+            msg = "TradeController/buy_order_check : Something strange."
+            trader.d_message(msg)
+            raise Exception(msg)
+        elif res[1] == "BUY":
+            amount = res[3] * comm
+            price = res[2] * Tradeconfig.sell_rate
 
-                    result = trader.sell_signal(amount, price, True)
+            result = trader.sell_signal(amount, price, True)
 
-                    if result[0]:
-                        trade_id[0] = True
-                        trade_id[1] = result[1]
-                        thread_flag = close_sold_check
-                        trader.d_message(
-                            "You bought BTC successfully and sent sell order\nsize: " +
-                            str(amount) +
-                            "\nprice: " +
-                            str(price))
-                    else:
-                        trader.d_message(
-                            "TradeController buy_order_check : Failed to send sell signal.")
-                        raise Exception(
-                            "TradeController buy_order_check : Failed to send sell signal.")
-        else:
+            if not result[0]:
+                msg = "TradeController buy_order_check : Failed to send sell signal."
+                trader.d_message(msg)
+                raise Exception(msg)
+            trade_id[0] = True
+            trade_id[1] = result[1]
+            thread_flag = close_sold_check
             trader.d_message(
-                "TradeController buy_order_check : Something strange.")
-            raise Exception(
-                "TradeController buy_order_check : Something strange.")
+                "You bought BTC successfully and sent sell order\nsize: " +
+                str(amount) +
+                "\nprice: " +
+                str(price))
 
     elif thread_flag == close_sold_check:
-        if trade_id[0]:
-            res = trader.get_order(trade_id[1])
-            if res[0] == "COMPLETED":
-                trade_id[0] = False
-                thread_flag = buy_jdg
-                trader.d_message("You sold BTC successfully.")
-                continue
-            amount = res[3] * comm
-            price = res[2] / Tradeconfig.sell_rate
-            if trader.shouldsellall(price):
-                trader.cancel_all_orders()
-                result_close = trader.close_position(amount, True)
-                if result_close[0]:
-                    trade_id[0] = False
-                    thread_flag = buy_jdg
-                    trader.d_message("やばいので撤退しますた")
-                else:
-                    trader.d_message(
-                        "!!!!!!!TradeController close_sold_check : Failed to retreat!!!!!!!!!!!.")
-                    trader.d_message(
-                        "!!!!!!!!!!TradeController close_sold_check : Failed to retreat!!!!!!!!!!!.")
-                    trader.d_message(
-                        "!!!!!!!!!TradeController close_sold_check : Failed to retreat!!!!!!!!!!!.")
-                    raise Exception(
-                        "TradeController close_sold_check : Failed to retreat!!!!!!!!!!!.")
+        if not trade_id[0]:
+            raise Exception("TradeController close_sold_check : Something strange.")
+        res = trader.get_order(trade_id[1])
+        if res[0] == "COMPLETED":
+            trade_id[0] = False
+            thread_flag = buy_jdg
+            trader.d_message("You sold BTC successfully.")
+            continue
+        amount = res[3] * comm
+        price = res[2] / Tradeconfig.sell_rate
+        if not trader.shouldsellall(price):
+            continue
+        trader.cancel_all_orders()
+        result_close = trader.close_position(amount, True)
+        if result_close[0]:
+            trade_id[0] = False
+            thread_flag = buy_jdg
+            trader.d_message("やばいので撤退しますた")
         else:
+            trader.d_message(
+                "!!!!!!!TradeController close_sold_check : Failed to retreat!!!!!!!!!!!.")
+            trader.d_message(
+                "!!!!!!!!TradeController close_sold_check : Failed to retreat!!!!!!!!!!!.")
+            trader.d_message(
+                "!!!!!!!!!TradeController close_sold_check : Failed to retreat!!!!!!!!!!!.")
             raise Exception(
-                "TradeController close_sold_check : Something strange.")
+                "TradeController close_sold_check : Failed to retreat!!!!!!!!!!!.")
