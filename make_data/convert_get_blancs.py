e940c30a6a464db6a548a2a343cfde37c05ee5a5
elzup <guild0105@gmail.com>
2020-08-11 21:00:41 +0900

    make merged black analyze

--- make_data/convert_get_blancs.py
+++ make_data/convert_get_blancs.py
@@ -3,19 +3,41 @@ import datetime
 
 # 設定値
 #   from https://api.bitcoincharts.com/v1/csv/
-input_file_name = "./data/bitflyerJPY.csv"
-output_file_name = "./data/btcjpn_skippedrange.csv"
+# output_file_name = "./data/btcjpn_skippedrange.csv"
+input_file_name1 = "./data/bitflyerJPY.csv"
+input_file_name2 = "./data/coincheckJPY.csv"
+output_file_name = "./data/btcjpn_skippedrange_merged.csv"
 # 集計を開始する日付＋時刻
 # 2017/07/04 17:01:38
-kijyun_date = datetime.datetime.strptime('2017-07-04 17:15:00', '%Y-%m-%d %H:%M:%S')
-kizami_date = datetime.timedelta(minutes=1)
+iter_date = datetime.datetime.strptime('2017-07-04 17:15:00', '%Y-%m-%d %H:%M:%S')
+diff_date = datetime.timedelta(minutes=1)
 
 
-# CSV読み込み
-csv_file = open(input_file_name, "r", encoding="ms932", errors="", newline="")
-f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
+def read_csv(path):
+    csv_file = open(path, "r", encoding="ms932", errors="", newline="")
+    return csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
 
-# 4本値を計算
+
+f1 = read_csv(input_file_name1)
+f2 = read_csv(input_file_name2)
+
+
+def merge_iter(f1, f2):
+    row1 = next(f1, None)
+    row2 = next(f2, None)
+    t1 = datetime.datetime.fromtimestamp(int(row1[0]))
+    t2 = datetime.datetime.fromtimestamp(int(row2[0]))
+    while row1 and row2:
+        if t1 < t2:
+            yield [t1, row1]
+            row1 = next(f1, None)
+            t1 = row1 and datetime.datetime.fromtimestamp(int(row1[0]))
+        else:
+            yield [t2, row2]
+            row2 = next(f2, None)
+            t2 = row2 and datetime.datetime.fromtimestamp(int(row2[0]))
+
+
 datalist = []
 priceO = 0
 priceH = 0
@@ -24,9 +46,9 @@ priceC = 0
 priceV = 0
 pre_datafull_date = None
 
-for row in f:
+for [time, row] in merge_iter(f1, f2):
     # forLoopで読み込まれたタイムスタンプに対応するローカルな日付 < 基準日 + ○分足
-    if (datetime.datetime.fromtimestamp(int(row[0])) < kijyun_date + kizami_date):
+    if (time < iter_date + diff_date):
         # Openにレートが入っているか。
         if priceO == 0:
             priceO = int(row[1].replace('.000000000000', ''))
@@ -53,20 +75,20 @@ for row in f:
             priceL = priceO
             priceC = priceO
             if pre_datafull_date is None:
-                pre_datafull_date = kijyun_date
+                pre_datafull_date = iter_date
 
         elif pre_datafull_date is not None:
-            diff = (kijyun_date - pre_datafull_date).total_seconds()
+            diff = (iter_date - pre_datafull_date).total_seconds()
             if int(diff) > 30 * 60:
-                datalist_new = str(pre_datafull_date), str(kijyun_date), diff
+                datalist_new = str(pre_datafull_date), str(iter_date), diff
                 datalist.append(datalist_new)
             pre_datafull_date = None
 
-        # datalist_new = '{0:%Y%m%d %H:%M:%S}'.format(kijyun_date + kizami_date), priceO, priceH, priceL, priceC, priceV
-        # datalist_new = int((kijyun_date + kizami_date).timestamp()), priceO, priceH, priceL, priceC, priceV
+        # datalist_new = '{0:%Y%m%d %H:%M:%S}'.format(iter_date + diff_date), priceO, priceH, priceL, priceC, priceV
+        # datalist_new = int((iter_date + diff_date).timestamp()), priceO, priceH, priceL, priceC, priceV
         # datalist.append(datalist_new)
         # 次ループの下処理
-        kijyun_date = kijyun_date + kizami_date
+        iter_date = iter_date + diff_date
         priceO = priceC
         priceH = 0
         priceL = 0
